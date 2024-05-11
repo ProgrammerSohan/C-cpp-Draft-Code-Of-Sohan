b5d95a32b9d55aa626c2ca656efafc453cb29157
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-05-12 02:58:19 +0600

    0_some basic codes

---
+++ main.cpp
@@ -0,0 +1,7 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+int main(){
+    cout <<"Hello Sohan, I am a Software Engineer!!"<<endl;
+}
---
+++ test1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int num1;
+    std::cin >> num1;
+    std::cout << num1 << std::endl;
+    return 0;
+
+}
