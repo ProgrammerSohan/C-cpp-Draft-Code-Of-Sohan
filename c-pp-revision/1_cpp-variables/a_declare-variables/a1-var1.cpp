99c169089747729563b9b93de030e809ab99037b
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-05-18 20:49:47 +0600

    03.1a_declare variables

---
+++ c-pp-revision/1_cpp-variables/a_declare-variables/a1-var1.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+/*using namespace std;
+
+int main(){
+    int myNum = 15;
+
+    cout<< myNum;
+    return 0;
+
+}*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int myNum = 25;
+    int hisNum = 15;
+    cout <<myNum<<endl;
+    cout <<hisNum;
+    return 0;
+
+}
