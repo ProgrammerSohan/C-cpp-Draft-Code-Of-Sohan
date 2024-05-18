74dd8c0bcf38a683c22e731f4bee90048812d7f6
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-05-18 21:00:25 +0600

    03.2b_declaring variable without value

---
+++ c-pp-revision/1_cpp-variables/a_declare-variables/a2-var2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int myNum;
+    double dblNum;
+    char chrNum;
+     string strNum;
+     bool blNum;
+    myNum = 99;
+    dblNum = 19.99;
+    chrNum = 's';
+    strNum = "Hello Sohan";
+    blNum = false;
+    //cout below
+    cout <<myNum<<endl;
+    cout <<dblNum<<endl;
+    cout <<chrNum<<endl;
+    cout <<strNum<<endl;
+    cout <<blNum;
+
+    return 0;
+}
