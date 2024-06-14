0d4fbf43096bc8a54415eb1b6e38ec7878266397
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-06-15 04:20:29 +0600

    bp_c_multiplication of-2num

---
+++ c-pp-revision/02_basic-program/c_multiplication-of-2num.cpp
@@ -0,0 +1,29 @@
+
+/*
+//1
+#include<iostream>
+using namespace std;
+int main(){
+ int a,b,c;
+ cout<<"Enter first number:\n";
+ cin>>a;
+ cout<<"Enter second number:\n";
+ cin>>b;
+ c=a*b;
+ cout<<"Multiply = "<<c <<endl;
+ return 0;
+}
+*/
+#include<iostream>
+using namespace std;
+int main(){
+ int a,b,c;
+ cout<<"Enter first number: \n";
+ cin>>a;
+ cout<<"Enter second number: \n";
+ cin>>b;
+ c=a*b;
+ cout<<"Multiply = "<<c <<endl;
+ return 0;
+
+}
