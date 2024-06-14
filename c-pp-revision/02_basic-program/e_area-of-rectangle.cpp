c1512bb7319ae8c0101f4a4cc42c5c2e69bae842
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-06-15 04:52:03 +0600

    bp_e_area-of-rectangle

---
+++ c-pp-revision/02_basic-program/e_area-of-rectangle.cpp
@@ -0,0 +1,28 @@
+/*
+//1
+#include<iostream>
+using namespace std;
+int main(){
+    int h,w,area;
+    cout<<"Enter height of rectangle: \n";
+    cin>>h;
+    cout<<"Enter width of rectangle: \n";
+    cin>>w;
+    area=h*w;
+    cout<<"Area of rectangle="<<area<<endl;
+    return 0;
+}
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  int h,w,area;
+  cout<<"Enter height of rectangle: \n";
+  cin>>h;
+  cout<<"Enter width of rectangle : \n";
+  cin>>w;
+  area=h*w;
+  cout<<"Area="<<area<<endl;
+  return 0;
+
+}
