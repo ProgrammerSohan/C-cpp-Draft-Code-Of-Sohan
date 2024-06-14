6a79f94b607e6c6537a4096fc7e4816198cacbd5
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-06-15 04:34:17 +0600

    bp_d_division of-2num

---
+++ c-pp-revision/02_basic-program/d_division-of-2num.cpp
@@ -0,0 +1,31 @@
+/*
+//1
+#include<iostream>
+using namespace std;
+int main(){
+ int a,b,c;
+ cout<<"Enter first number: \n";
+ cin>>a;
+ cout<<"Enter second number: \n";
+ cin>>b;
+ c=a/b;
+ cout<<"Division= "<< c <<endl;
+ return 0;
+
+}
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter 1st num: \n";
+    cin>>a;
+    cout<<"enter 2nd num: \n";
+    cin>>b;
+    c=a/b;
+    cout<<"division = "<< c <<endl;
+    return 0;
+
+
+
+}
